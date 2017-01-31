396f66b8598545412733d72b6cfaf7d8a1b5cbde
Joshua Parsels <joshparsels@gmail.com>
2017-01-30 18:20:52 -0800

    Added Startup Files

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -7,7 +8,16 @@ using stupid.Factory;
 namespace stupid
 {
     public class Startup
+
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         public IConfiguration Configuration { get; private set; }
         public void ConfigureServices(IServiceCollection services)
